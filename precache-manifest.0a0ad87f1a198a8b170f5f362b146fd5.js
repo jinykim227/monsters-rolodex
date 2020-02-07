8339c5a11a88def8b1c4334e584c9b71ceef466a
Jin Kim <jinkim227@gmail.com>
2020-02-07 14:37:08 -0800

    Updates

---
+++ precache-manifest.0a0ad87f1a198a8b170f5f362b146fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e35283e6acc5b7ec96386e1ef7d18cc",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3ad85a03d9ca93d8d6e6",
+    "url": "/github.io/monsters-rolodex/static/css/main.8eef69de.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ad85a03d9ca93d8d6e6",
+    "url": "/github.io/monsters-rolodex/static/js/main.e972d857.chunk.js"
+  },
+  {
+    "revision": "b6030ce2f0898b5ec3f8",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.5868f27a.js"
+  }
+]);
